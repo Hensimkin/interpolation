3b63804120c3eeabde832e714e7f0363a1315d94
Hen Simkin <hensi1@ac.sce.ac.il>
2022-06-05 17:17:42 +0300

    1 liniar 2 polynom

--- interpolation.py
+++ interpolation.py
@@ -24,9 +24,22 @@ def polynom(table,point):
     if len(table)<3:
         print("cant calculate")
     else:
-
+        i=0
         mat=[[1],[1],[1]]
         vec=[[],[],[]]
+
+        while table[i][0]<point and table[i+1][0]<point and table[i+2][0]<point:
+            i+=1
+        mat[0].append(table[i][0])
+        mat[0].append((table[i][0]) ** 2)
+        mat[1].append(table[i + 1][0])
+        mat[1].append((table[i + 1][0]) ** 2)
+        mat[2].append(table[i + 2][0])
+        mat[2].append((table[i + 2][0]) ** 2)
+        vec[0].append(table[i][1])
+        vec[1].append(table[i + 1][1])
+        vec[2].append(table[i + 2][1])
+        """""
         a=choice(table)
         if a==table[len(table)-2] or a==table[len(table)-1]:
             a=table[len(table)-3]
@@ -47,8 +60,8 @@ def polynom(table,point):
         mat[2].append(c[0])
         mat[2].append(c[0] ** 2)
         vec[2].append(c[1])
-        """""
-        mat=[[1,1,1],[1,2,4],[1,3,9]]
+        
+        #mat=[[1,1,1],[1,2,4],[1,3,9]]
         vec=[[0.8415],[0.9093],[0.1411]]
         mat=getMatrixInverse(mat)
         
@@ -81,10 +94,10 @@ def getMatrixDeternminant(m):
     return determinant
 
 def getMatrixInverse(m):
-    determinant = getMatrixDeternminant(m)
+    determinant = round(getMatrixDeternminant(m),6)
     if len(m) == 2:
-        return [[m[1][1]/determinant, -1*m[0][1]/determinant],
-                [-1*m[1][0]/determinant, m[0][0]/determinant]]
+        return [[round(m[1][1]/determinant,6), round(-1*(m[0][1]/determinant),6)],
+                [round(-1*(m[1][0]/determinant),6), round(m[0][0]/determinant,6)]]
     cofactors = []
     for r in range(len(m)):
         cofactorRow = []
@@ -113,15 +126,188 @@ def multiply(v, G):
     for i in range(len(G[0])):
         total = 0
         for j in range(len(v)):
-            total += v[j] * G[j][i]
+            a=round((G[j][i])*(v[j]),6)
+            total = total+a
+        result.append(total)
+    return result
+
+
+
+def multiply2(v, G):
+    result = []
+    for i in range(len(G[0])):
+        total = 0
+        for j in range(len(v)):
+            a=round((G[i][j])*(v[j]),6)
+            total = round(total+a,5)
         result.append(total)
     return result
 
 
 
 
+
+def transposeMatrix(m):
+    a= list(map(list,zip(*m)))
+    return a
+
+def getMatrixMinor(m,i,j):
+    return [row[:j] + row[j+1:] for row in (m[:i]+m[i+1:])]
+
+def getMatrixDeternminant(m):
+    #base case for 2x2 matrix
+    if len(m) == 2:
+        return m[0][0]*m[1][1]-m[0][1]*m[1][0]
+
+    determinant = 0
+    for c in range(len(m)):
+        determinant += ((-1)**c)*m[0][c]*getMatrixDeternminant(getMatrixMinor(m,0,c))
+    return determinant
+
+
+
+
+def lagrange(table,point):
+    pn=0
+    for i in range(len(table)):
+        ln=0
+        for j in range(len(table)):
+            if i!=j:
+                if ln==0:
+                  ln+=(point-table[j][0])/(table[i][0]-table[j][0])
+                else:
+                    ln *= (point - table[j][0]) / (table[i][0] - table[j][0])
+        pn+=table[i][1]*ln
+    print("The value of f(",point,") is :",round(pn,4))
+
+def nevile(table, point):
+    myarr=[[0 for col in range(len(table))]for row in range(len(table))]
+    for i in range(len(table)):
+        if i==0:
+            for j in range(len(table)):
+                myarr[j][j]=table[j][1]
+        else:
+            for k in range(len(table)):
+                if k+i<len(table):
+                 myarr[k][k+i]=(((point-table[k][0])*myarr[k+1][k+i])-((point-table[k+i][0])*myarr[k][k+i-1]))/(table[k+i][0]-table[k][0])
+    print("The value of f(", point, ") is :",round(myarr[0][len(table)-1],5) )
+
+
+
+
+
+
+def splinecubic(table,point):
+    distances=[]
+    a=0
+    b=0
+    for i in range(len(table)-1):
+        distances.append(round(table[i+1][0]-table[i][0],6))
+        if table[i][0]<point and table[i+1][0]>point:
+            a=i
+            b=i+1
+    matrix = [[0 for col in range(len(table)-2)] for row in range(len(table)-2)]
+    d=[]
+    print(distances)
+    for j in range(len(matrix)):
+        for k in range(len(matrix)):
+            if k==j:
+                matrix[j][k]=2
+            if k==j+1 and j+1<len(matrix):
+                matrix[j][k]=round(distances[j+1]/(distances[j]+distances[j+1]),6)
+            if k+1==j and k+1<len(matrix):
+                matrix[j][k]=round(1-(distances[j+1]/(distances[j]+distances[j+1])),6)
+        ck=round(6/round((distances[j]+distances[j+1]),5),6)
+        ck2=round(((table[j+2][1]-table[j+1][1])/distances[j+1])-((table[j+1][1]-table[j][1])/distances[j]),6)
+        ck3=round(ck*ck2,6)
+        #d.append((6/(distances[j]+distances[j+1]))*(((table[j+2][1]-table[j+1][1])/distances[j+1])-((table[j+1][1]-table[j][1])/distances[j])))
+        d.append(ck3)
+    print(matrix)
+    print(d)
+
+    matrix=getMatrixInverse(matrix)
+    s=multiply2(d,matrix)
+    print(s)
+    if a==0:
+        mi=0
+    else:
+        mi=s[a-1]
+    if b+1==len(table):
+        mi1=0
+    else:
+        mi1=s[b-1]
+    print(mi, mi1)
+
+
+    x=(((((table[b][0]-point)**3)*mi)+((point-table[a][0])**3)*mi1))/(6*distances[a-1])
+    print(round(x,5))
+    y=(((table[b][0]-point)*table[a][1])+((point-table[a][0])*table[b][1]))/distances[a]
+    print(round(y,5))
+
+
+    z=(((table[b][0]-point)*mi)+(((point-table[a][0])*mi1)*distances[a-1]))/6
+    print(round(z,5))
+    """""
+    x=((((table[b][0]-point)**3)*mi)+(((point-table[a][0])**3)*mi1))/(6*distances[1])
+    y=(((table[b][0]-point)*table[a][1])/distances[2])
+    z=((((table[b][0]-point)*mi)+((point-table[a][0])*mi1))*distances[2])/6
+    """""
+    sk=round(x+y-z,6)
+    print("The value of f(", point, ") is :",round(sk,5))
+
+
+
+
+
+
+
+
+
+"""""
 mat3=[[1,1,1],[1,2,4],[1,3,9]]
 arr=[[0,0],[1,0.8415],[2,0.9093],[3,0.1411],[4,-0.7568],[5,-0.9585],[6,-0.2794]]
 point=2.5
 #linear(arr,point)
 polynom(arr,point)
+"""""
+
+
+
+def main():
+    #print("Choose :\n 1.Liniar \n2.polynom \n3.lagrange\n4.nevile\n5.splinecubic\n6.exit")
+    a=-2
+    #a=int(input())
+    while a!=200:
+        print("Choose :\n 1.Liniar \n2.polynom \n3.lagrange\n4.nevile\n5.splinecubic\n6.exit")
+        a = int(input())
+        if a == 1:
+            arr = [[0, 0], [1, 0.8415], [2, 0.9093], [3, 0.1411], [4, -0.7568], [5, -0.9585], [6, -0.2794]]
+            point = 2.5
+            linear(arr, point)
+        elif a == 2:
+            arr = [[0, 0], [1, 0.8415], [2, 0.9093], [3, 0.1411], [4, -0.7568], [5, -0.9585], [6, -0.2794]]
+            point = 2.5
+            polynom(arr, point)
+        elif a == 3:
+            arr3 = [[1, 1], [2, 0], [4, 1.5]]
+            point2 = 3
+            lagrange(arr3, point2)
+        elif a == 4:
+            arr5 = [[1, 0], [1.2, 0.112463], [1.3, 0.167996], [1.4, 0.222709]]
+            point = 1.28
+            nevile(arr5, point)
+        elif a == 5:
+            arr2 = [[0, 0], [round(pie / 6, 5), 0.5], [round(pie / 4, 5), 0.7072], [round(pie / 2, 5), 1]]
+            point = round(pie / 3, 5)
+            splinecubic(arr2, point)
+        elif a == 6:
+            a=200
+            print("goodbye")
+        elif a>6:
+            print("Wrong choice")
+
+
+
+pie=3.14159
+main()
+
